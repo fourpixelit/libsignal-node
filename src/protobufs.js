a14cfacfb5044dd88a2f87d696c69235a108aaa7
Diego Araujo <diegoaraujoart@hotmail.com>
2021-08-23 22:48:42 -0300

    groups

--- src/protobufs.js
+++ src/protobufs.js
@@ -4,9 +4,12 @@ const path = require('path');
 const protobuf = require('protobufjs');
 
 const protodir = path.resolve(__dirname, '../protos/');
-const p = protobuf.loadSync(path.join(protodir, 'WhisperTextProtocol.proto')).lookup('textsecure');
+const whisper = protobuf.loadSync(path.join(protodir, 'WhisperTextProtocol.proto')).lookup('textsecure');
+const local = protobuf.loadSync(path.join(protodir, 'LocalStorageProtocol.proto')).lookup('textsecure');
 
 module.exports = {
-    WhisperMessage: p.lookup('WhisperMessage'),
-    PreKeyWhisperMessage: p.lookup('PreKeyWhisperMessage')
+    WhisperMessage: whisper.lookup('WhisperMessage'),
+    PreKeyWhisperMessage: whisper.lookup('PreKeyWhisperMessage'),
+    SenderKeyDistributionMessage: whisper.lookup('SenderKeyDistributionMessage'),
+    SenderKeyMessage: whisper.lookup('SenderKeyMessage'),
 };
