6a5f8983e9b630e42211f7267dd13f9ae75a297c
Diego Araujo <diegoaraujoart@hotmail.com>
2021-08-24 00:18:33 -0300

    grous

--- src/group_session_builder.js
+++ src/group_session_builder.js
@@ -1,6 +1,7 @@
 //const utils = require('../../common/utils');
 //const SenderKeyDistributionMessage = require('../protocol/SenderKeyDistributionMessage');
 
+const keyhelper = require("./keyhelper");
 class GroupSessionBuilder {
   constructor(senderKeyStore) {
     this.senderKeyStore = senderKeyStore;
@@ -19,15 +20,15 @@ class GroupSessionBuilder {
   }
 
   // [{"senderKeyId":1742199468,"senderChainKey":{"iteration":0,"seed":"yxMY9VFQcXEP34olRAcGCtsgx1XoKsHfDIh+1ea4HAQ="},"senderSigningKey":{"public":""}}]
-  /*async create(senderKeyName) {
+  async create(senderKeyName) {
     try {
       const senderKeyRecord = await this.senderKeyStore.loadSenderKey(senderKeyName);
       console.log('GroupSessionBuilder create session', senderKeyName, senderKeyRecord);
 
       if (senderKeyRecord.isEmpty()) {
-        const keyId = utils.generateSenderKeyId();
-        const senderKey = utils.generateSenderKey();
-        const signingKey = utils.generateSenderSigningKey();
+        const keyId = keyhelper.generateSenderKeyId();
+        const senderKey = keyhelper.generateSenderKey();
+        const signingKey = keyhelper.generateSenderSigningKey();
 
         senderKeyRecord.setSenderKeyState(keyId, 0, senderKey, signingKey);
         await this.senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord);
@@ -45,6 +46,6 @@ class GroupSessionBuilder {
       console.log(e.stack);
       throw new Error(e);
     }
-  }*/
+  }
 }
 module.exports = GroupSessionBuilder;
